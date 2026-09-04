ea05af858213dea21f83409a84eae96809891df2
Dhanushree12340 <ugcet2502718@reva.edu.in>
2026-09-04 15:06:18 +0530

    Add greet() function - paired with devikany632-afk via Live Share

--- hello.c
+++ hello.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+void greet(const char *name)
+{
+    printf("Hello, %s! Welcome to your GitHub portfolio.\n", name);
+}
+
 int main()
 {
-    printf("Hello World\n");
+    printf("Hello World!\n");
+
+    greet("Ada");
+
     return 0;
 }
